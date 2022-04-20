4f513893350c89b3acf33dd70bc8917026262231
Jerika Kanz <jerikacapila@gmail.com>
2022-04-20 19:58:49 +0200

    Mapping Components

--- 18-mapping-components/src/App.js
+++ 18-mapping-components/src/App.js
@@ -1,37 +1,9 @@
 import Joke from "./components/Joke";
+import jokesData from "./jokesData";
 
 export default function App() {
-  return (
-    <div>
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm"
-      />
-
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-
-      <Joke
-        punchline="It’s hard to explain puns to kleptomaniacs because 
-they always take things literally."
-      />
-    </div>
-  );
+  const jokeElements = jokesData.map((joke) => (
+    <Joke setup={joke.setup} punchline={joke.punchline} />
+  ));
+  return <div>{jokeElements}</div>;
 }
