f2eb881473a5f47efa45f03a7b76a6f7a01bb3f9
Jerika Kanz <jerikacapila@gmail.com>
2022-04-21 14:59:28 +0200

    Project: Spread object as props

--- 25-spred-object/src/App.js
+++ 25-spred-object/src/App.js
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 import data from "./data";
 
 export default function App() {
-  const cards = data.map((el) => <Card key={el.id} el={el} />);
+  const cards = data.map((el) => <Card key={el.id} {...el} />);
 
   return (
     <div>
--- 25-spred-object/src/components/Card.js
+++ 25-spred-object/src/components/Card.js
@@ -1,8 +1,8 @@
 export default function Card(props) {
   let badgeText;
-  if (props.el.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.el.location === "Online") {
+  } else if (props.location === "Online") {
     badgeText = "ONLINE";
   }
 
@@ -11,19 +11,19 @@ export default function Card(props) {
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
         className="card--photo"
-        src={require(`../images/${props.el.coverImg}`)}
+        src={require(`../images/${props.coverImg}`)}
         alt="Swimmer"
       />
       <p>
         <span className="star">★ </span>
-        {props.el.stats.rating}{" "}
+        {props.stats.rating}{" "}
         <span>
-          ({props.el.stats.reviewCount}) • {props.el.location}
+          ({props.stats.reviewCount}) • {props.location}
         </span>
       </p>
-      <p>{props.el.title}</p>
+      <p>{props.title}</p>
       <p>
-        <strong>From ${props.el.price}</strong> / person
+        <strong>From ${props.price}</strong> / person
       </p>
     </div>
   );
