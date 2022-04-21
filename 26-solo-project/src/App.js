48857eec3a80810daca70491f1c9a3d6ca7f1ed5
Jerika Kanz <jerikacapila@gmail.com>
2022-04-21 20:01:53 +0200

    Adding Class Name

--- 26-solo-project/src/App.js
+++ 26-solo-project/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Article from "./components/Article";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Article />
     </div>
   );
 }
